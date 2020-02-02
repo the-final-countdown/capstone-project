e3fbb857b03a3077660ede7a3c786bf97a9841f9
Austin Malmberg <austin.malmberg@gmail.com>
2020-02-01 23:39:26 -0500

    Removed create_app method; refactored

--- app.py
+++ app.py
@@ -2,43 +2,38 @@ import os
 
 from flask import Flask
 
+import db
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY = 'dev',
-        DATABASE = os.path.join(app.instance_path, 'capstone-project.sqlite')
-    )
+# route blueprints
+from routes import portfolio
+from routes import auth
+from routes import admin
 
-    if test_config is None:
-        # load the instance config when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load test config
-        app.config.from_mapping(test_config)
 
-    # create the instance folder, if necessary
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY = 'dev',
+    DATABASE = os.path.join(app.instance_path, 'capstone-project.sqlite')
+)
 
-    import db
-    db.init_app(app)
+# create the instance folder, if necessary
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    # route for main page
-    from routes import portfolio
-    app.register_blueprint(portfolio.bp)
-    app.add_url_rule('/', endpoint='index')
+# initialize the db
+db.init_app(app)
 
-    # routes to login and register
-    from routes import auth
-    app.register_blueprint(auth.bp)
+# ~~~~~~ ROUTES ~~~~~~~
 
-    # route to admin page
-    from routes import admin
-    app.register_blueprint(admin.bp)
+# main page
+app.register_blueprint(portfolio.bp)
+app.add_url_rule('/', endpoint='index')
 
-    # return the newly created app
-    return app
+# login and registration
+app.register_blueprint(auth.bp)
+
+# admin
+app.register_blueprint(admin.bp)
