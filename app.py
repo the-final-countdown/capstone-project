03c6c0ee34de4729d1cca4cbe2b70ef6255308f7
Nathan <nathanmundo@icloud.com>
2020-02-04 22:41:53 -0500

    Testing out postgress setup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from models import User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -10,6 +11,24 @@ db = SQLAlchemy(app)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route("/add")
+def add_user():
+    email = request.args.get('email')
+    password = request.args.get('password')
+    timestamp = request.args.get('timestamp')
+    try:
+        user = User(
+            email=email,
+            password=password,
+            timestamp=timestamp
+        )
+        db.session.add(user)
+        db.session.commit()
+        return "Book added. user id={}".format(user.id)
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.Text, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    first_name = db.Column(db.Text)
+    last_name = db.Column(db.Text)
+    address = db.Column(db.Text)
+    city = db.Column(db.Text)
+    state = db.Column(db.Text)
+    zip_code = db.Column(db.Text)
+    telephone = db.Column(db.Text)
+    created_on = db.Column(db.TIMESTAMP, nullable=False)
