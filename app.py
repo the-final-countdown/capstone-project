af0b056e8db1f752fdc8d1491139b413b61e9743
Austin Malmberg <austin.malmberg@gmail.com>
2020-02-11 21:55:00 -0500

    Refactored SQLALCHEMY_DATABASE_URI

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev',
-        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///capstone-project.sqlite',
+        SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL', 'sqlite:///capstone-project.sqlite'),
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
