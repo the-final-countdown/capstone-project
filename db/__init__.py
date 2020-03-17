f8af02280ff45d4af678622424880cc83ddf3f70
LTSchmiddy <schmidalex1996@gmail.com>
2020-03-17 18:31:51 -0400

    populate stocks and stock history. Base version. needs polishing.

--- db/__init__.py
+++ db/__init__.py
@@ -11,41 +11,90 @@ from werkzeug.security import generate_password_hash
 dba = SQLAlchemy()
 
 from db.tables import *
+from db import stock_handler as sth
+
 
 def init_app(app):
     with app.app_context():
         dba.init_app(app)
         dba.create_all()
+
+        # Adding click commands:
         app.cli.add_command(populate_users)
+        app.cli.add_command(populate_stocks)
+        app.cli.add_command(populate_stock_history)
 
 
-@click.command('populate-db')
+@click.command('populate-users')
 @with_appcontext
 def populate_users():
-    url = f"https://my.api.mockaroo.com/users.json?key={os.environ.get('API_KEY_MOCKAROO')}"
+    _process_mockaroo_query("https://my.api.mockaroo.com/users.json", create_user, "users")
 
-    response = requests.get(url)
+    # url = f"https://my.api.mockaroo.com/users.json?key={os.environ.get('API_KEY_MOCKAROO')}"
+    #
+    # response = requests.get(url)
+    #
+    # # only add users if successful response
+    # if response.status_code == 200:
+    #
+    #     json_users = response.json()
+    #
+    #     errors = 0
+    #
+    #     for user in json_users:
+    #         error = create_user(user)
+    #         if error is not None:
+    #             errors += 1
+    #
+    #     users_added = len(json_users) - errors
+    #
+    #     # return a summary and print it to console
+    #     click.echo(f'Added {users_added} users ({errors} errors)')
+    #     return
+    #
+    # # display erroneous responses
+    # click.echo(response.json())
 
-    # only add users if successful response
-    if response.status_code == 200:
 
-        json_users = response.json()
+@click.command('populate-stocks')
+@with_appcontext
+def populate_stocks():
+    _process_mockaroo_query("https://my.api.mockaroo.com/stock_symbols.json", create_stock, "stocks")
 
-        errors = 0
+@click.command('populate-stock-data')
+@with_appcontext
+def populate_stock_history():
+    for stock in dba.engine.execute("SELECT * FROM STOCK").fetchall():
+        rId = stock[0]
+        rSymbol = stock[1]
+        rName = stock[2]
 
-        for user in json_users:
-            error = create_user(user)
-            if error is not None:
-                errors += 1
+        history = sth.fetch_stock_data(rSymbol)
 
-        users_added = len(json_users) - errors
+        # if history.
 
-        # return a summary and print it to console
-        click.echo(f'Added {users_added} users ({errors} errors)')
-        return
+        if len(history.index) <= 0:
+            click.echo(rSymbol + " not found. Skipping... ")
+            continue
 
-    # display erroneous responses
-    click.echo(response.json())
+        click.echo("generating for " + rSymbol + "... ")
+        for i in history.index:
+
+            new_history_dict = {
+                "stock_id": rId,
+                "date": sth.timestamp_to_date(str(i)),
+                "high": history['High'][i],
+                "low": history['Low'][i],
+                "open": history['Open'][i],
+                "close": history['Close'][i]
+            }
+
+            create_stock_history(new_history_dict)
+
+
+
+    # dba.session.commit()
+
 
 
 # Models
@@ -118,3 +167,79 @@ def get_all_users():
     Returns all users
     """
     return User.query.order_by(User.last_name, User.first_name).all()
+
+
+
+
+def create_stock(stock):
+
+    if 'stock_symbol' not in stock:
+        return 'Stock symbol required'
+
+    stock_name = ""
+    if 'stock_name' not in stock:
+        stock_name = sth.get_company_name_for_symbol(stock['stock_symbol'])
+    else:
+        stock_name = stock['stock_name']
+
+    new_stock = Stock(stock_symbol=stock['stock_symbol'], company_name=stock_name)
+
+    dba.session.add(new_stock)
+    dba.session.commit()
+
+def create_stock_history(stock_history):
+
+    # if 'stock_id' not in stock_history and 'stock_symbol' not in stock_history:
+    #     return 'stock_id or stock_symbol required'
+    if 'stock_id' not in stock_history:
+        return 'stock_id required'
+    elif 'date' not in stock_history:
+        return 'date required'
+    elif 'high' not in stock_history:
+        return 'high required'
+    elif 'low' not in stock_history:
+        return 'low required'
+    elif 'open' not in stock_history:
+        return 'open required'
+    elif 'close' not in stock_history:
+        return 'close required'
+
+
+    new_history = Stock_History(
+        fk_stock_id=stock_history['stock_id'],
+        date=stock_history['date'],
+        high=stock_history['high'],
+        low=stock_history['low'],
+        open=stock_history['open'],
+        close=stock_history['close']
+    )
+
+    dba.session.add(new_history)
+    dba.session.commit()
+
+
+def _process_mockaroo_query(dataUrl, processing_function, entry_type="entries"):
+    url = dataUrl + f"?key={os.environ.get('API_KEY_MOCKAROO')}"
+
+    response = requests.get(url)
+
+    # only add users if successful response
+    if response.status_code == 200:
+
+        json_users = response.json()
+
+        errors = 0
+
+        for user in json_users:
+            error = processing_function(user)
+            if error is not None:
+                errors += 1
+
+        users_added = len(json_users) - errors
+
+        # return a summary and print it to console
+        click.echo(f'Added {users_added} ' + entry_type + f' ({errors} errors)')
+        return
+
+    # display erroneous responses
+    click.echo(response.json())
---
+++ db/stock_handler.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import datetime as dt
+from pandas_datareader import data as pdr
+from tabulate import tabulate
+import sqlite3
+import requests
+
+
+default_start_year = 2020
+default_start_month = 1
+default_start_day = 1
+
+def get_default_start_date():
+    return dt.date(default_start_year, default_start_month, default_start_day)
+
+
+
+def fetch_stock_data(stock_symbol: str, start:dt.date = get_default_start_date(), end:dt.date = dt.date.today()):
+    yf.pdr_override()
+
+    df = pdr.get_data_yahoo(stock_symbol, start, end)
+
+    return df
+
+def timestamp_to_date(stamp:str):
+    dateArr = stamp.split(" ")[0].split("-")
+    return dt.date(int(dateArr[0]), int(dateArr[1]), int(dateArr[2]))
+
+
+
+def get_company_name_for_symbol(stock_symbol:str):
+    url = "http://d.yimg.com/autoc.finance.yahoo.com/autoc?query={}&region=1&lang=en".format(stock_symbol)
+
+    result = requests.get(url).json()
+
+    for x in result['ResultSet']['Result']:
+        if x['symbol'] == stock_symbol:
+            return x['name']
+
